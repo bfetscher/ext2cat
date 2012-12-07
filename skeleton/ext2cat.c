dca8da282bf0ee5fa60d507f9368efa5911c1adc
Burke Fetscher <bfetscher@gmail.com>
2012-12-06 20:32:20 -0600

    do the extra credit stuff (indirect block) as well

--- skeleton/ext2cat.c
+++ skeleton/ext2cat.c
@@ -41,6 +41,18 @@ int main(int argc, char ** argv) {
         bytes_read += bytes_to_read;
     }
 
+    // also get stuff from the singly indirect block, if possible
+    void * indir_block = get_block(fs, target_ino->i_block[EXT2_IND_BLOCK]);
+    __u32 * next_block = (__u32 *)indir_block;
+    while (bytes_read < size && (((void *)next_block - indir_block) < get_block_size(fs))) {
+      bytes_left = size - bytes_read;
+       __u32 bytes_to_read = bytes_left > block_size ? block_size : bytes_left;
+       void * block = get_block(fs, *next_block);
+       memcpy(buf + bytes_read, block, bytes_to_read);
+       bytes_read += bytes_to_read;
+       next_block++;
+    }
+
     write(1, buf, bytes_read);
     if (bytes_read < size) {
         printf("%s: file uses indirect blocks. output was truncated!\n",
